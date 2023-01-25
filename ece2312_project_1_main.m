bc38e3d712792467701da7d7927c2f873c60621c
Trevor Drane <twdrane@wpi.edu>
2023-01-25 15:32:03 -0500

    create spectrogram function

--- ece2312_project_1_main.m
+++ ece2312_project_1_main.m
@@ -1,16 +1,33 @@
 function ece2312_project_1_main
 
 recordLength = 5;
-micname = audiodevinfo(1,0)
-audiodevinfo(1,0,44100,24,1)
-audiodevinfo(0,2)
+audiodevinfo(1,0);
+audiodevinfo(1,0,44100,24,1);
+audiodevinfo(0,2);
 
-recorder = audiorecorder(44100,24,1,0)
+recorder = audiorecorder(44100,24,1,0);
 disp('Start')
-recordblocking(recorder,recordLength)
+recordblocking(recorder,recordLength);
 disp('Finished')
 
-player = audioplayer(recorder,2)
-playblocking(player,recordLength)
+player = audioplayer(recorder,2);
+playblocking(player,recordLength);
+
+recording = getaudiodata(recorder);
+
+makeSpectrogram(recording);
+
+% window = hamming(512);
+% N_overlap = 256;
+% N_fft = 1024;
+% [S,F,P,T] = spectrogram(recording,window,N_overlap,N_fft,44100,'yaxis');
+% figure;
+% surf(T,F,10*log10(P),'EdgeColor','none');
+% axis tight;
+% view(0,90);
+% colormap(jet);
+% set(gca,'clim',[-80,-20]);
+% ylim([0 8000]);
+% xlabel('Time (s)');ylabel('Frequency (Hz)');
 
 return
---
+++ makeSpectrogram.m
@@ -0,0 +1,17 @@
+function makeSpectrogram(speech_data)
+% A function to create a spectrogram of a recording of 
+%   
+window = hamming(512);
+N_overlap = 256;
+N_fft = 1024;
+[S,F,P,T] = spectrogram(speech_data,window,N_overlap,N_fft,44100,'yaxis');
+figure;
+surf(T,F,10*log10(P),'edgecolor','none');
+axis tight;
+view(0,90);
+colormap(jet);
+set(gca,'clim',[-80,-20]);
+ylim([0 8000]);
+xlabel('Time (s)');ylabel('Frequency (Hz)');
+
+return
