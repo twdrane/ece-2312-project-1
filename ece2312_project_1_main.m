e6e7ff78d72869c5818cf2a94aeade86fd9eeead
Trevor Drane <twdrane@wpi.edu>
2023-01-25 14:43:19 -0500

    create recording part

--- ece2312_project_1_main.m
+++ ece2312_project_1_main.m
@@ -1,5 +1,16 @@
 function ece2312_project_1_main
 
+recordLength = 5;
+micname = audiodevinfo(1,0)
+audiodevinfo(1,0,44100,24,1)
+audiodevinfo(0,2)
 
+recorder = audiorecorder(44100,24,1,0)
+disp('Start')
+recordblocking(recorder,recordLength)
+disp('Finished')
+
+player = audioplayer(recorder,2)
+playblocking(player,recordLength)
 
 return
