a0cb86dc2eca610af85b4ff472a604b59594ee18
Trevor Drane <twdrane@wpi.edu>
2023-01-25 19:42:16 -0500

    Fixed Z must be a matrix in makeSpectrogram[s,f,t,p]

--- ece2312_project_1_main.m
+++ ece2312_project_1_main.m
@@ -6,12 +6,13 @@ audiodevinfo(1,0,44100,24,1);
 audiodevinfo(0,2);
 
 recorder = audiorecorder(44100,24,1,0);
-disp('Start')
+disp('Start recording')
 recordblocking(recorder,recordLength);
-disp('Finished')
+disp('Recording finished. Playing back')
 
 player = audioplayer(recorder,2);
 playblocking(player,recordLength);
+disp('Done')
 
 recording = getaudiodata(recorder);
 audiowrite('recording1.wav',recording,44100);
@@ -21,7 +22,7 @@ makeSpectrogram(recording);
 % window = hamming(512);
 % N_overlap = 256;
 % N_fft = 1024;
-% [S,F,P,T] = spectrogram(recording,window,N_overlap,N_fft,44100,'yaxis');
+% [S,F,T,P] = spectrogram(recording,window,N_overlap,N_fft,44100,'yaxis');
 % figure;
 % surf(T,F,10*log10(P),'EdgeColor','none');
 % axis tight;
--- makeSpectrogram.m
+++ makeSpectrogram.m
@@ -4,9 +4,9 @@ function makeSpectrogram(speech_data)
 window = hamming(512);
 N_overlap = 256;
 N_fft = 1024;
-[S,F,P,T] = spectrogram(speech_data,window,N_overlap,N_fft,44100,'yaxis');
+[S,F,T,P] = spectrogram(speech_data,window,N_overlap,N_fft,44100,'yaxis');
 figure;
-surf(T,F,10*log(P),'edgecolor','none');
+surf(T,F,10*log10(P),'edgecolor','none');
 axis tight;
 view(0,90);
 colormap(jet);
