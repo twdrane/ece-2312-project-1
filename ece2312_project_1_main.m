3e34687b43c8f93ef46ac6ce78a051808a477f17
Trevor Drane <twdrane@wpi.edu>
2023-01-30 13:14:25 -0500

    merge makeSpectrogram into one file

--- ece2312_project_1_main.m
+++ ece2312_project_1_main.m
@@ -1,9 +1,8 @@
-function ece2312_project_1_main
 
-recordLength = 5;
-audiodevinfo(1,0);
+recordLength = 1; %set the length of the recording in seconds
+audiodevinfo(1,0); 
 audiodevinfo(1,0,44100,24,1);
-audiodevinfo(0,2);
+audiodevinfo(0,2)
 
 recorder = audiorecorder(44100,24,1,0);
 disp('Start recording')
@@ -32,4 +31,23 @@ makeSpectrogram(recording);
 % ylim([0 8000]);
 % xlabel('Time (s)');ylabel('Frequency (Hz)');
 
-return
+
+
+function makeSpectrogram(audio_data)
+
+% A function to create a spectrogram of an audio recording
+
+window = hamming(512);
+N_overlap = 256;
+N_fft = 1024;
+[~,F,T,P] = spectrogram(audio_data,window,N_overlap,N_fft,44100,'yaxis');
+figure;
+surf(T,F,10*log10(P),'edgecolor','none');
+axis tight;
+view(0,90);
+colormap(jet);
+set(gca,'clim',[-80,-20]);
+ylim([0 8000]);
+xlabel('Time (s)');ylabel('Frequency (Hz)');
+
+end
