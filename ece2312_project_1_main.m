e9d28673e542503a4a137a2dcf4e6e27cff548ad
Trevor Drane <twdrane@wpi.edu>
2023-01-30 13:32:26 -0500

    tidy up structure and add comments

--- ece2312_project_1_main.m
+++ ece2312_project_1_main.m
@@ -1,36 +1,22 @@
 
-recordLength = 1; %set the length of the recording in seconds
-audiodevinfo(1,0); 
-audiodevinfo(1,0,44100,24,1);
-audiodevinfo(0,2)
+recordLength = 5; %set the length of the recording in seconds
+audiodevinfo(1,0); %verify microphone
+audiodevinfo(1,0,44100,24,1); %check sample frequency, bit storage, and chanels
+audiodevinfo(0,2); %verify speaker
 
-recorder = audiorecorder(44100,24,1,0);
+recorder = audiorecorder(44100,24,1,0); %create audio recording object
 disp('Start recording')
-recordblocking(recorder,recordLength);
+recordblocking(recorder,recordLength); %record for recordLength seconds
 disp('Recording finished. Playing back')
 
-player = audioplayer(recorder,2);
-playblocking(player,recordLength);
+player = audioplayer(recorder,2); %create playback object
+playblocking(player,recordLength); %playback recorded sample
 disp('Done')
 
-recording = getaudiodata(recorder);
+recording = getaudiodata(recorder); %convert recorder object into an audio file
 audiowrite('recording1.wav',recording,44100);
 
-makeSpectrogram(recording);
-
-% window = hamming(512);
-% N_overlap = 256;
-% N_fft = 1024;
-% [S,F,T,P] = spectrogram(recording,window,N_overlap,N_fft,44100,'yaxis');
-% figure;
-% surf(T,F,10*log10(P),'EdgeColor','none');
-% axis tight;
-% view(0,90);
-% colormap(jet);
-% set(gca,'clim',[-80,-20]);
-% ylim([0 8000]);
-% xlabel('Time (s)');ylabel('Frequency (Hz)');
-
+makeSpectrogram(recording); %graph audio as a spectrogram
 
 
 function makeSpectrogram(audio_data)
