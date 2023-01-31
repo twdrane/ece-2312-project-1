7ace42664af023e6974f481c1abc5ce6b3534b0e
Trevor Drane <twdrane@wpi.edu>
2023-01-31 14:15:13 -0500

    add audio files and audio file playback capability

--- ece2312_project_1_main.m
+++ ece2312_project_1_main.m
@@ -1,33 +1,44 @@
 
 recordLength = 5; %set the length of the recording in seconds
 audiodevinfo(1,0); %verify microphone
-audiodevinfo(1,0,44100,24,1); %check sample frequency, bit storage, and chanels
+audiodevinfo(1,0,44100,24,1) %check sample frequency, bit storage, and chanels
 audiodevinfo(0,2); %verify speaker
 
-recorder = audiorecorder(44100,24,1,0); %create audio recording object
+recorder = audiorecorder(44100,24,1,0); %create audio recording object, 44100Hz, 24bit, mono or stereo, 0(default microphone)
 disp('Start recording')
 recordblocking(recorder,recordLength); %record for recordLength seconds
 disp('Recording finished. Playing back')
 
-player = audioplayer(recorder,2); %create playback object
+%[audioFile,Fs] = audioread('ivorybuckles.wav'); %read audio file
+%player = audioplayer(audioFile,Fs,24,2); %create playback object from a previously recorded file
+
+player = audioplayer(recorder,2); %create playback object from recording object
+
 playblocking(player,recordLength); %playback recorded sample
 disp('Done')
 
 recording = getaudiodata(recorder); %convert recorder object into an audio file
 audiowrite('recording1.wav',recording,44100);
 
-makeSpectrogram(recording); %graph audio as a spectrogram
-
+makeSpectrogram(recording); %graph recorded audio as a spectrogram
+%makeSpectrogram(audioFile); %graph audio file as a spectrogram
 
 function makeSpectrogram(audio_data)
 
-% A function to create a spectrogram of an audio recording
+% A function to create a spectrogram of an audio recording (with time plot)
+
+t = [0:length(audio_data)-1] / 44100;
+subplot(2,1,1)
+plot(t, audio_data) 
+title('Amplitude vs Time'); 
+ylabel('Amplitude'); 
+xlabel('Time (Seconds)');
 
 window = hamming(512);
 N_overlap = 256;
 N_fft = 1024;
 [~,F,T,P] = spectrogram(audio_data,window,N_overlap,N_fft,44100,'yaxis');
-figure;
+subplot(2,1,2)
 surf(T,F,10*log10(P),'edgecolor','none');
 axis tight;
 view(0,90);
