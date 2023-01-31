2494fcc661ec71cc929ee01f448f012549d88381
Trevor Drane <twdrane@wpi.edu>
2023-01-31 16:15:37 -0500

    improve comment divisions

--- ece2312_project_1_main.m
+++ ece2312_project_1_main.m
@@ -4,35 +4,33 @@ audiodevinfo(1,0); %verify microphone
 audiodevinfo(1,0,44100,24,1); %check sample frequency, bit storage, and chanels
 audiodevinfo(0,2); %verify speaker
 
+%% comment the lines below for audio file playback
 recorder = audiorecorder(44100,24,1,0); %create audio recording object, 44100Hz, 24bit, mono or stereo, 0(default microphone)
 disp('Start recording')
+recordblocking(recorder,recordLength); %record for recordLength seconds
+disp('Recording finished.')
+
+%% comment through line 18 for recording playback
+% [audioFile,Fs] = audioread('ivorybuckles.wav'); %read audio file
+% %the next 2 lines convert the audio to one sided stereo, comment calls for makeSpectrogram
+% emptyChannel = zeros([220500,1]); 
+% audioFile = cat(2,audioFile,emptyChannel);
+% player = audioplayer(audioFile,Fs,24,2); %create playback object from a previously recorded file
 
 %% comment the line below for audio file playback
-%recordblocking(recorder,recordLength); %record for recordLength seconds
+player = audioplayer(recorder,2); %create playback object from recording object
 
-disp('Recording finished. Playing back')
-
-%% comment through line 22 for recording playback
-[audioFile,Fs] = audioread('ivorybuckles.wav'); %read audio file
-
-%the next 2 lines convert the audio to one sided stereo
-emptyChannel = zeros([220500,1]); 
-audioFile = cat(2,audioFile,emptyChannel);
-
-player = audioplayer(audioFile,Fs,24,2); %create playback object from a previously recorded file
-
-%% comment the line below for audio file playback
-%player = audioplayer(recorder,2); %create playback object from recording object
-
+%% always needed
+disp('Playing back')
 playblocking(player,recordLength); %playback recorded sample
 disp('Done')
 
 %% comment these lines for audio file playback
-%recording = getaudiodata(recorder); %convert recorder object into an audio file
-%audiowrite('recording1.wav',recording,44100);
+recording = getaudiodata(recorder); %convert recorder object into an audio file
+audiowrite('recording1.wav',recording,44100);
 
 %% pick the graph for the desired output, do not use during stereo
-%makeSpectrogram(recording); %graph recorded audio as a spectrogram
+makeSpectrogram(recording); %graph recorded audio as a spectrogram
 %makeSpectrogram(audioFile); %graph audio file as a spectrogram
 
 %%
