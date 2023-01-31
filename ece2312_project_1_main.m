017fbd9a14fea2feb68da5754475b35207218b3f
Trevor Drane <twdrane@wpi.edu>
2023-01-31 14:46:51 -0500

    add stereo playback thing

--- ece2312_project_1_main.m
+++ ece2312_project_1_main.m
@@ -1,26 +1,30 @@
 
 recordLength = 5; %set the length of the recording in seconds
 audiodevinfo(1,0); %verify microphone
-audiodevinfo(1,0,44100,24,1) %check sample frequency, bit storage, and chanels
+audiodevinfo(1,0,44100,24,1); %check sample frequency, bit storage, and chanels
 audiodevinfo(0,2); %verify speaker
 
 recorder = audiorecorder(44100,24,1,0); %create audio recording object, 44100Hz, 24bit, mono or stereo, 0(default microphone)
 disp('Start recording')
-recordblocking(recorder,recordLength); %record for recordLength seconds
+
+%recordblocking(recorder,recordLength); %record for recordLength seconds
+
 disp('Recording finished. Playing back')
 
-%[audioFile,Fs] = audioread('ivorybuckles.wav'); %read audio file
-%player = audioplayer(audioFile,Fs,24,2); %create playback object from a previously recorded file
+[audioFile,Fs] = audioread('ivorybuckles.wav'); %read audio file
+emptyChannel = zeros([220500,1]);
+audioFile = cat(2,audioFile,emptyChannel);
+player = audioplayer(audioFile,Fs,24,2); %create playback object from a previously recorded file
 
-player = audioplayer(recorder,2); %create playback object from recording object
+%player = audioplayer(recorder,2); %create playback object from recording object
 
 playblocking(player,recordLength); %playback recorded sample
 disp('Done')
 
-recording = getaudiodata(recorder); %convert recorder object into an audio file
-audiowrite('recording1.wav',recording,44100);
+%recording = getaudiodata(recorder); %convert recorder object into an audio file
+%audiowrite('recording1.wav',recording,44100);
 
-makeSpectrogram(recording); %graph recorded audio as a spectrogram
+%makeSpectrogram(recording); %graph recorded audio as a spectrogram
 %makeSpectrogram(audioFile); %graph audio file as a spectrogram
 
 function makeSpectrogram(audio_data)
